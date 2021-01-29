c41ac55ce2abafb2cd3d75eea53962c4a0c61f39
uhziel <uhziel@gmail.com>
2021-01-29 22:42:55 +0800

    添加'添加书源'功能，添加对操作错误时的提示

--- src/store/modules/bookSources.js
+++ src/store/modules/bookSources.js
@@ -2,6 +2,7 @@ import {graphql} from "../../api.js"
 
 const state = () => ({
   all: [],
+  error: "",
 });
 
 const getters = {
@@ -10,6 +11,9 @@ const getters = {
 const mutations = {
   setBookSources(state, bookSources) {
     state.all = bookSources;
+  },
+  setError(state, error) {
+    state.error = error;
   },
 };
 
@@ -25,55 +29,70 @@ const actions = {
       commit('setBookSources', res.data.data.bookSources);
     }).catch(e => console.error(e));
   },
-  deleteBookSource({dispatch}, payload) {
+  addBooksource({commit, dispatch}, payload) {
+    const query = `
+      mutation CreateBookSource($downloadUrl: String!) {
+        createBookSource(downloadUrl: $downloadUrl) {
+          id name url version enableSearch
+        }
+      }`;
+    graphql(query, payload).then(res => {
+      if (!res.data.errors) {
+        dispatch('fetchBookSources');
+      } else {
+        commit('setError', res.data.errors[0].message);
+      }
+    }).catch(e => console.error(e));
+  },
+  deleteBooksource({commit, dispatch}, payload) {
     const query = `
       mutation DeleteBookSource($id: ID!) {
         deleteBookSource(id: $id)
       }`;
     graphql(query, payload).then(res => {
-      if (res.data.data && res.data.data.deleteBookSource === true) {
+      if (!res.data.errors) {
         dispatch('fetchBookSources');
       } else {
-        //TODO
+        commit('setError', res.data.errors[0].message);
       }
     }).catch(e => console.error(e));
   },
-  moveUpBooksource({dispatch}, payload) {
+  moveUpBooksource({commit, dispatch}, payload) {
     const query = `
       mutation MoveUpBookSource($id: ID!) {
         moveUpBookSource(id: $id)
       }`;
     graphql(query, payload).then(res => {
-      if (res.data.data && res.data.data.moveUpBookSource === true) {
+      if (!res.data.errors) {
         dispatch('fetchBookSources');
       } else {
-        //TODO
+        commit('setError', res.data.errors[0].message);
       }
     }).catch(e => console.error(e));
   },
-  moveDownBooksource({dispatch}, payload) {
+  moveDownBooksource({commit, dispatch}, payload) {
     const query = `
       mutation MoveDownBookSource($id: ID!) {
         moveDownBookSource(id: $id)
       }`;
     graphql(query, payload).then(res => {
-      if (res.data.data && res.data.data.moveDownBookSource === true) {
+      if (!res.data.errors) {
         dispatch('fetchBookSources');
       } else {
-        //TODO
+        commit('setError', res.data.errors[0].message);
       }
     }).catch(e => console.error(e));
   },
-  enableBooksource({dispatch}, payload) {
+  enableBooksource({commit, dispatch}, payload) {
     const query = `
       mutation EnableBookSource($id: ID!, $value: Boolean) {
         enableSearchBookSource(id: $id, value: $value)  
       }`;
     graphql(query, payload).then(res => {
-      if (res.data.data && res.data.data.enableSearchBookSource === true) {
+      if (!res.data.errors) {
         dispatch('fetchBookSources');
       } else {
-        //TODO
+        commit('setError', res.data.errors[0].message);
       }
     }).catch(e => console.error(e));    
   }
--- src/views/BookSources.vue
+++ src/views/BookSources.vue
@@ -1,5 +1,12 @@
 <template>
   <b-container class="bookSources">
+    <b-row>
+      <b-col>
+        <b-form-input type="text" v-model="newBooksourceUrl" size="sm" class="mr-sm-2"></b-form-input>
+        <b-button size="sm" class="my-2 my-sm-0" @click.prevent="addBooksource">添加书源</b-button>
+        <b-alert variant="danger" class="mt-3" :show="error.length > 0">{{error}}</b-alert>
+      </b-col>
+    </b-row>
     <b-row cols="1" cols-sm="2" cols-xl="3">
       <b-col v-for="(booksource, index) in bookSources" :key="index">
         <BookSourceInfo :content="booksource" :index="index" />
@@ -14,15 +21,19 @@ import BookSourceInfo from "@/components/BookSourceInfo"
 export default {
   name: "BookSources",
   components: {
-    BookSourceInfo
+    BookSourceInfo,
   },
   data() {
     return {
+      newBooksourceUrl: "",
     };
   },
   computed: {
     bookSources() {
       return this.$store.state.bookSources.all;
+    },
+    error() {
+      return this.$store.state.bookSources.error;
     },
   },
   created() {
@@ -34,6 +45,15 @@ export default {
     }
   },
   methods: {
+    addBooksource() {
+      if (this.newBooksourceUrl.length > 0 ) {
+        this.$store.dispatch({
+          type: 'addBooksource',
+          downloadUrl: this.newBooksourceUrl,
+        });
+        this.newBooksourceUrl = "";
+      }
+    },
     fetchBookSources() {
       this.$store.dispatch('fetchBookSources');
     }
