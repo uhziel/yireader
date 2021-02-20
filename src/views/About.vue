d42f7c13b05ee8f10138b852939960cb885ed645
uhziel <uhziel@gmail.com>
2021-02-20 20:11:10 +0800

    在关于中显示 yireader 的版本号

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,20 @@
 <template>
   <div class="about">
     <p>易读是一款简单易用的个人小说阅读器。</p>
+    <p>yireader 版本: v{{version}}</p>
     <p>项目地址：<a href="https://github.com/uhziel/yireader" target="_blank">https://github.com/uhziel/yireader</a></p>
   </div>
 </template>
+
+<script>
+import {version} from '../version';
+
+export default {
+  name: 'About',
+  data() {
+    return {
+      version,
+    };
+  },
+}
+</script>
