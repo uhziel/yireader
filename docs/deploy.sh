029df87bfd0cadf0f09253d1bcc25c098a5d59d4
uhziel <uhziel@gmail.com>
2021-02-22 00:01:59 +0800

    docs/ 里的 deploy.sh 针对 Github Actions  runner 环境做修改

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -12,14 +12,17 @@ cd src/.vuepress/dist
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
+git config --global init.defaultBranch main
+git config --global user.email 'uhziel@gmail.com'
+git config --global user.name 'uhziel'
 git init
 git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:uhziel/yireader.git master:gh-pages
+git push -f git@github.com:uhziel/yireader.git main:gh-pages
 
 cd -
